796f52da10c50f688af26306b394229d7a4facdb
Isha Bhansali <ishab2@uw.edu>
2024-05-16 16:50:43 -0700

    Rounded the values

--- summarytable.R
+++ summarytable.R
@@ -9,23 +9,16 @@ library(dplyr)
 data <- read.csv("dataset.csv")
 
 
-# View the first few rows of the data frame
-head(data)
-# Group by track_genre and artists
-grouped_data <- data %>%
-  group_by(track_genre, artists)
-
-# Summarize the grouped data to see the results
-summary_data <- grouped_data %>%
+summary_data <- data %>%
+  group_by(track_genre) %>%
   summarize(
-    avg_popularity = mean(popularity, na.rm = TRUE),
-    avg_duration = mean(duration_ms, na.rm = TRUE),
-    avg_danceability = mean(danceability, na.rm = TRUE),
-    avg_energy = mean(energy, na.rm = TRUE),
-    avg_loudness = mean(loudness, na.rm = TRUE),
-    avg_tempo = mean(tempo, na.rm = TRUE),
+    avg_popularity = round(mean(popularity, na.rm = TRUE), 0),
+    avg_duration_min = round (mean(duration_ms, na.rm = TRUE) / 60000, 0),
+    avg_danceability = round(mean(danceability, na.rm = TRUE), 0),
+    avg_energy = round(mean(energy, na.rm = TRUE), 0),
+    avg_loudness = round(mean(loudness, na.rm = TRUE), 0),
+    avg_tempo = round(mean(tempo, na.rm = TRUE), 0),
     track_count = n()
-  )
-
-# View the summarized data
+  ) %>%
+  arrange(desc(avg_popularity))
 print(summary_data)
